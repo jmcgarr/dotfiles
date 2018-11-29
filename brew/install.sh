348f16fb7f6943e7b479e685eb395d9e5cacbc0c
Mike McGarr <mmcgarr@slack-corp.com>
2018-11-29 12:55:36 -0800

    Making changes for new environment

--- brew/install.sh
+++ brew/install.sh
@@ -2,6 +2,7 @@
 if ! test $(which brew); then
   echo "  Installing Homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  brew tap AdoptOpenJDK/openjdk
   brew tap caskroom/versions
   brew tap homebrew/versions
 fi
