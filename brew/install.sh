c07c2c174d0fe4deb11686d4b3d0609726b8fb3f
Mike McGarr <mmcgarr@slack-corp .com>
2021-02-22 10:55:37 -0800

    Saving machine changes before switching machines

--- brew/install.sh
+++ brew/install.sh
@@ -2,6 +2,7 @@
 if ! test $(which brew); then
   echo "  Installing Homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+  # TODO this doesn't work and not sure why...yet. This tap isn't added during bootstrap.sh
   brew tap AdoptOpenJDK/openjdk
   brew tap caskroom/versions
   brew tap homebrew/versions
